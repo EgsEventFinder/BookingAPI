fb87d724545cf44d21f8abd796649d3a814b82db
Miguel Tavares <miguel.tavares07@ua.pt>
2023-03-08 00:11:26 +0000

    Minor changes

--- api/app.py
+++ api/app.py
@@ -1,105 +1,157 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 import yaml
+import datetime
 
 app = Flask(__name__)
 
-db = yaml.load(open('db.yaml'))
+db = yaml.load(open('db.yaml'), Loader=yaml.Loader)
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
- 
-mysql = MySQL(app)
 
+mysql = MySQL(app)
+ 
 @app.route("/ticket", methods=["POST"])
 def book_ticket():
-    cur = mysql.connection.cursor()
-
     ticket_data = request.get_json()
     id = ticket_data["id"]
     user_id = ticket_data["userId"]
-    booking_date = datetime.date.today()
+    
+    with mysql.connection.cursor() as cur:
+        try:
+            cur.execute("SELECT * FROM tickets WHERE id=%s AND booked=False", (id,))
+            ticket = cur.fetchone()
 
-    cur.execute("SELECT * FROM tickets WHERE id=%s AND booked=False", (id,))
-    ticket = cur.fetchone()
+            if ticket is None:
+                return jsonify({"message": "Ticket not available or already booked"}), 400
+            
+            booking_date = datetime.date.today()
+            cur.execute("INSERT INTO booked_tickets (id, bookingDate, userId) VALUES (%s, %s, %s)", (id, booking_date, user_id))
 
-    if ticket is None:
-        return jsonify({"message": "Ticket not available or already booked"}), 400
+            cur.execute("UPDATE tickets SET booked=True WHERE id=%s", (id,))
+            mysql.connection.commit()
 
-    cur.execute("INSERT INTO booked_tickets (ticketId, bookingDate, userId) VALUES (%s, %s, %s)", (id, booking_date, user_id))
+            return jsonify({"message": "Ticket booked"})
 
-    cur.execute("UPDATE tickets SET booked=True WHERE id=%s", (id,))
-    mysql.connection.commit()
+        except:
+            mysql.connection.rollback()
+            return jsonify({"message": "Error booking ticket"}), 500
 
-    return jsonify({"message": "Ticket booked"})
+        finally:
+            cur.close()
 
+@app.route("/ticket/<id>", methods=["PUT"])
+def unbook_ticket(id):
+    with mysql.connection.cursor() as cur:
+        try:
+            cur.execute("SELECT * FROM booked_tickets WHERE id=%s", (id,))
+            booking = cur.fetchone()
 
-@app.route("/ticket/<ticket_id>", methods=["PUT"])
-def unbook_ticket(ticket_id):
-    cur = mysql.connection.cursor()
+            if booking is None:
+                return jsonify({"message": "Ticket not found"}), 404
 
-    cur.execute("SELECT * FROM booked_tickets WHERE id=%s", (id,))
-    booking = cur.fetchone()
+            cur.execute("DELETE FROM booked_tickets WHERE id=%s", (id,))
 
-    if booking is None:
-        return jsonify({"message": "Ticket not found"}), 404
+            cur.execute("UPDATE tickets SET booked=False WHERE id=%s", (id,))
+            mysql.connection.commit()
 
-    cur.execute("DELETE FROM booked_tickets WHERE ticketId=%s", (ticket_id,))
+            return jsonify({"message": "Ticket unbooked"})
 
-    cur.execute("UPDATE tickets SET booked=False WHERE id=%s", (ticket_id,))
-    mysql.connection.commit()
+        except:
+            mysql.connection.rollback()
+            return jsonify({"message": "Error unbooking ticket"}), 500
 
-    return jsonify({"message": "Ticket unbooked"})
+        finally:
+            cur.close()
 
+@app.route("/ticket/<id>", methods=["GET"])
+def get_ticket(id):
+    user_id = 1
+    
+    with mysql.connection.cursor() as cur:
+        try:
+            query = """
+                SELECT b.bookingDate, t.price, t.eventId, t.type FROM booked_tickets as b 
+                    INNER JOIN tickets as t ON b.id = t.id WHERE b.id = %s AND b.userId = %s
+            """
+            cur.execute(query,(id,user_id,))
+            booking = cur.fetchone()
 
-@app.route("/ticket/<ticket_id>", methods=["GET"])
-def get_ticket(ticket_id):
-    cur = mysql.connection.cursor()
+            if booking is None:
+                return jsonify({"message": "Ticket not found"}), 404
 
-    cur.execute("SELECT * FROM booked_tickets WHERE ticketId=%s", (ticket_id,))
-    booking = cur.fetchone()
+            return jsonify(booking)
 
-    if booking is None:
-        return jsonify({"message": "Ticket not found"}), 404
-
-    return jsonify(booking)
-
+        except:
+            return jsonify({"message": "Error getting ticket"}), 500
+        
+        finally:
+            cur.close()
 
 @app.route("/tickets", methods=["GET"])
 def get_tickets():
     user_id = 1
+    
     page = int(request.args.get("page", 1))
     limit = int(request.args.get("limit", 10))
     offset = (page - 1) * limit
+    
+    with mysql.connection.cursor() as cur:
+        try:
+            query = """
+                SELECT b.bookingDate, t.price, t.eventId, t.type FROM booked_tickets as b 
+                INNER JOIN tickets as t ON b.id = t.id WHERE b.userId = %s LIMIT %s, %s"
+            """
+            cur.execute(query,(user,offset,limit,))
+            user_tickets = cur.fetchall()
 
-    cur = mysql.connection.cursor()
+            return jsonify(user_tickets)
 
-    cur.execute("SELECT * FROM booked_tickets WHERE userId=%s LIMIT %s, %s", (user_id, offset, limit))
-    user_tickets = cur.fetchall()
+        except:
+            return jsonify({"message": "Error getting user tickets"}), 500
 
-    return jsonify(user_tickets)
-
+        finally:
+            cur.close()
 
 @app.route("/event/<event_id>/tickets", methods=["GET"])
 def get_event_tickets(event_id):
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM tickets WHERE eventId=%s", (event_id,))
-    event_tickets = cur.fetchall()
+    with mysql.connection.cursor() as cur:
+        try:
+            cur.execute("SELECT * FROM tickets WHERE eventId=%s and booked=False", (event_id,))
+            event_tickets = cur.fetchall()
 
-    return jsonify(event_tickets)
+            return jsonify(event_tickets)
 
-'''
+        except:
+            return jsonify({"message": "Error getting event tickets"}), 500
+
+        finally:
+            cur.close()
+
+
 @app.route("/tickets/<event_id>", methods=["GET"])
-def get_event_tickets(event_id):
-    cur = mysql.connection.cursor()
+def get_number_event_tickets(event_id):
+    with mysql.connection.cursor() as cur:
+        try:
+            query = """
+                SELECT COUNT(*) AS num_booked_tickets FROM tickets 
+                    INNER JOIN booked_tickets ON tickets.id = booked_tickets.id 
+                        WHERE tickets.eventId = %s AND tickets.booked = True
+            """
+            cur.execute(query,(event_id))
+            result = cur.fetchone()
+            count = result[0]
 
-    cur.execute("SELECT COUNT(*) FROM booked_tickets WHERE eventId=%s", (event_id,))
-    result = cur.fetchone()
-    count = result[0]
+            return jsonify({"count": count})
 
-    return jsonify({"count": count})
-'''
+        except:
+            return jsonify({"message": "Error getting event tickets"}), 500
+
+        finally:
+            cur.close()
+
 if __name__ == '__main__':
     app.run(debug=True)
 
--- api/test.py
+++ api/test.py
@@ -5,70 +5,92 @@ from datetime import date
 app = Flask(__name__)
 
 tickets = [
-    {"id":1, "ticketId": 1, "eventId": 1, "price": 50, "type": "normal", "booked": True},
-    {"id":2, "ticketId": 2, "eventId": 1, "price": 100, "type": "VIP", "booked": True},
-    {"id":3, "ticketId": 1, "eventId": 2, "price": 10, "type": "normal", "booked": False},
-    {"id":4, "ticketId": 2, "eventId": 2, "price": 20, "type": "VIP", "booked": False},
-    {"id":5, "ticketId": 1, "eventId": 3, "price": 10, "type": "normal", "booked": False},
-    {"id":6, "ticketId": 2, "eventId": 3, "price": 20, "type": "VIP", "booked": False},
+    {"ticket_id":1, "ticket_number": 1, "event_id": 1, "price": 50, "type": "normal", "booked": True},
+    {"ticket_id":2, "ticket_number": 2, "event_id": 1, "price": 100, "type": "VIP", "booked": True},
+    {"ticket_id":3, "ticket_number": 1, "event_id": 2, "price": 10, "type": "normal", "booked": False},
+    {"ticket_id":4, "ticket_number": 2, "event_id": 2, "price": 20, "type": "VIP", "booked": False},
+    {"ticket_id":5, "ticket_number": 1, "event_id": 3, "price": 10, "type": "normal", "booked": False},
+    {"ticket_id":6, "ticket_number": 2, "event_id": 3, "price": 20, "type": "VIP", "booked": False},
 ]
 
 booked_tickets = [
-    {"bookingId": 1, "id": 1, "userId": 1, "bookingDate": "2022, 3, 7"},
-    {"bookingId": 2, "id": 2, "userId": 1, "bookingDate": "2022, 3, 7"},
+    {"booking_id": 1, "ticket_id": 1, "user_id": 1, "booking_date": "2022, 3, 7"},
+    {"booking_id": 2, "ticket_id": 2, "user_id": 1, "booking_date": "2022, 3, 7"},
 ]
 
 @app.route("/ticket", methods=["POST"])
 def book_ticket():
     ticket_data = request.get_json()
     for ticket in tickets:
-        if ticket["id"] == ticket_data["id"]:
-            if any(t["id"] == ticket_data["id"] and t["userId"] == ticket_data["userId"] for t in booked_tickets):
+        if ticket["ticket_id"] == ticket_data["ticket_id"]:
+            if any(t["ticket_id"] == ticket_data["ticket_id"] and t["user_id"] == ticket_data["user_id"] for t in booked_tickets):
                 # Ticket is already booked by the specified user
                 return jsonify({"message": "Ticket is already booked by specified user"}), 400
             else:
                 ticket["booked"] = True
-                booking_id = len(booked_tickets) + 1  # generate a new booking id
+                booking_id = len(booked_tickets) + 1  
                 booking_date = date.today()  
-                booked_tickets.append({"bookingId": booking_id, "id": ticket["id"], "userId": ticket_data["userId"], "bookingDate": booking_date})
+                booked_tickets.append({"booking_id": booking_id, "ticket_id": ticket["ticket_id"], "user_id": ticket_data["user_id"], "booking_date": booking_date})
+                print(booked_tickets)
                 return jsonify({"message": "Ticket booked"})
     return jsonify({"message": "Ticket not available"}), 400
 
-@app.route("/ticket/<id>", methods=["PUT"])
-def unbook_ticket(id):
+@app.route("/ticket/<ticket_id>", methods=["PUT"])
+def unbook_ticket(ticket_id):
     for ticket in booked_tickets:
-        if ticket["id"] == int(id):
+        if ticket["ticket_id"] == int(ticket_id):
             for t in tickets:
-                if t["id"] == int(id):
+                if t["ticket_id"] == int(ticket_id):
                     t["booked"] = False
                     break
             booked_tickets.remove(ticket)
             return jsonify({"message": "Ticket unbooked"})
     return jsonify({"message": "Ticket not found"}), 404
 
-@app.route("/ticket/<id>", methods=["GET"])
-def get_ticket(id):
-    user_id = 1
-    for ticket in booked_tickets:
-        if ticket["id"] == int(id):
-            return jsonify(ticket)
+@app.route("/ticket/<ticket_id>", methods=["GET"])
+def get_ticket(ticket_id):
+    user_id = 1 # or request.args.get("user_id")
+    for booking in booked_tickets:
+        if booking["ticket_id"] == int(ticket_id) and booking["user_id"] == user_id:
+            
+            ticket = next((t for t in tickets if t["ticket_id"] == int(ticket_id)), None)
+            if ticket:
+               
+                user_ticket = {
+                    "event_id": ticket["event_id"],
+                    "price": ticket["price"],
+                    "type": ticket["type"],
+                    "booking_date": booking["booking_date"]
+                }
+            
+                return jsonify(user_ticket)
     return jsonify({"message": "Ticket not found"}), 404
 
 @app.route("/tickets", methods=["GET"])
 def get_tickets():
-    #user_id = request.args.get("userId")
-    user_id = 1
+    user_id = 1 # or request.args.get("user_id")
     user_tickets = []
-    for ticket in booked_tickets:
-        if ticket["userId"] == user_id:
-            user_tickets.append(ticket)
+    for booking in booked_tickets:
+        if booking["user_id"] == user_id:
+            
+            ticket = next((t for t in tickets if t["ticket_id"] == booking["ticket_id"]), None)
+            if ticket:
+                
+                user_ticket = {
+                    "event_id": ticket["event_id"],
+                    "price": ticket["price"],
+                    "type": ticket["type"],
+                    "booking_date": booking["booking_date"]
+                }
+                
+                user_tickets.append(user_ticket)
     return jsonify(user_tickets)
 
 @app.route("/event/<event_id>/tickets", methods=["GET"])
 def get_event_tickets(event_id):
     event_tickets = []
     for ticket in tickets:
-        if ticket["eventId"] == int(event_id) and not ticket["booked"]:
+        if ticket["event_id"] == int(event_id) and not ticket["booked"]:
             event_tickets.append(ticket)
     return jsonify(event_tickets)
 
