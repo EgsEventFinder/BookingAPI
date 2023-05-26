b08065ebdd6a59a24b80609b98c290f37c7f1395
Miguel Tavares <miguel.tavares07@ua.pt>
2023-05-26 11:37:53 +0100

    Deploy files

--- api/app.py
+++ api/app.py
@@ -446,4 +446,4 @@ if __name__ == '__main__':
     while not connect_db():
         continue
     setup()
-    app.run(debug = True, host = '0.0.0.0', port = 8080)
+    app.run(debug = True, host = '0.0.0.0', port = 5002)
